48bb6eacfd450cc5bab469d65d3f354a776ee828
Juan Camilo Chambo Alzate <1109666058@u.icesi.edu.co>
2023-08-14 11:26:40 -0500

    Version 2 del hola mundo

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("¨Hola mundo!¨ en diferentes idiomas: ");
+
+        System.out.println("Hola món!");
+        System.out.println("שלום לך עולם!");
+        System.out.println("Olá, mundo!");
+        System.out.println("Chào thế giới!");
+
+    }
+}
